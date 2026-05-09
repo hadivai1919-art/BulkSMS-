de634cc98cd57e21c9b487a1130b3880feaee066
hadivai1919-art <hadivai1919@gmail.com>
2026-05-09 16:31:44 +0600

    Delete tg.php

--- tg.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Telegram প্রোফাইলে রিডাইরেক্ট করার PHP স্ক্রিপ্ট
-$telegram_url = "https://t.me/hadi_vai1";
-
-// HTTP হেডার ব্যবহার করে রিডাইরেক্ট
-header("Location: " . $telegram_url);
-exit();
-?>
