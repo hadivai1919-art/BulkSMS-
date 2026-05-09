42e6ccee099ad57d243bf0d9228f119637d7e815
hadivai1919-art <hadivai1919@gmail.com>
2026-05-09 16:23:29 +0600

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // সব সেশন ভ্যারিয়েবল মুছে ফেলে
-session_destroy(); // সেশন সম্পূর্ণভাবে ধ্বংস করে
-
-// ইউজারকে লগইন পেজে রিডাইরেক্ট করে
-header("Location: login.php");
-exit();
-?>
